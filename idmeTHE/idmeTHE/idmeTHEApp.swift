3c28e015570ef2fa70509ff468fdd12c34504fc0
mmohamed <irshad.mohamedfarook@insight.com>
2022-01-18 11:03:12 -0500

    initial commit

---
+++ idmeTHE/idmeTHE/idmeTHEApp.swift
@@ -0,0 +1,21 @@
+//
+//  idmeTHEApp.swift
+//  idmeTHE
+//
+//  Created by Mohamed Farook, Irshad on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct idmeTHEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+//TODO: -
+// Add environment and mvvm in readme
+//
---
+++ idmeTHE/idmeTHEUITests/idmeTHEUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  idmeTHEUITestsLaunchTests.swift
+//  idmeTHEUITests
+//
+//  Created by Mohamed Farook, Irshad on 1/18/22.
+//
+
+import XCTest
+
+class idmeTHEUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
